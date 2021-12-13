5d33e718fd76817b3156bee3f26290efffa077dc
LenaMazurenko <lena@rent-it.su>
2021-12-13 11:15:53 +0200

    starts files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+//import Form from './components/Form';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  // handleIncrement = type => {
+  //   this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  // };
+
+  render() {
+    // const { contacts, name } = this.state;
+    return <></>;
+  }
+}
+export default App;
